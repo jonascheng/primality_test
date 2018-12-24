5026be69b942546e0bb3dde917b33cea94d69c93
Jonas Cheng <jonas.cheng@gmail.com>
2018-12-24 19:18:34 +0800

    refine fermat

--- fermat_primality.py
+++ fermat_primality.py
@@ -8,14 +8,12 @@ def is_prime(n: int) -> bool:
     elif n % 2 == 0 or n % 3 == 0:
         return False
 
-    # determin test list
-    A = range(2, n - 2)
     # determin k
-    k = int(len(A) / 2)
+    k = 10
 
     for _ in range(k):
-        a = random.choice(A)
-        if a ** (n - 1) % n != 1:
+        a = random.randint(2, n - 2)
+        if (a ** (n - 1)) % n != 1:
             return False
 
     return True
